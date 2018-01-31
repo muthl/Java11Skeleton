985d3276a3e0c1b43409c26c3effcef879506ef8
mah man <muthl@github.com>
2018-01-31 11:13:52 -0800

    Assignment 1 Liam

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,16 @@
-// Student name:
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Best animal");
+        System.out.println("Best Pokemon");
+        System.out.println("10 + 10");
+        System.out.println("Who killed JFK");
+        System.out.println("If Denmark is a real country");
+        System.out.println("Submission date");
+    }
+}// Student name:
 // This is where your first assignment goes!
