f58159a17c9db4c4ccb04b68b975e618c70d071b
mah man <muthl@github.com>
2018-05-09 10:16:39 -0700

    fixed

---
+++ Assignment4/Question5.java
@@ -0,0 +1,8 @@
+public class Question5 {
+    public static void main(String[] args) {
+        double price = 1.47;
+        for (int i = 1; i < 11; i++) {
+            System.out.println ( i + " Items = $" + i * price);
+        }
+    }
+}
