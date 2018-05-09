8c4a7662d527a1fb4a07720427145b784c283be0
mah man <muthl@github.com>
2018-05-09 10:55:44 -0700

    fixes again

--- Assignment6/Age.java
+++ Assignment6/Age.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 class Age {
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    long age, days, months, dPM, dY, math;
-    System.out.println("What is your age in years?");
-    age = scan.nextInt();
+        Scanner scan = new Scanner(System.in);
+        long age, days, months, dPM, hours;
+        System.out.println("What is your age in years?");
+        age = scan.nextInt();
     
-    days = 365;
-    months = 12;
-    dPM = 31;
-    dY = age * days;
+        days = 365;
+        months = 12;
+        hours = 8760;
     
-    System.out.println("You are " + age * days + " days old");
-    System.out.println("You are " + dY/dPM + " Months " + dY % dPM + " and days old." );
+        System.out.println("You are " + age * months + " months old" );
+        System.out.println("You are " + age * days + " days old");
+        System.out.println("You are " + age * hours + " hours old");
     
     
     
