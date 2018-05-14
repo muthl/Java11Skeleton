119a37dc1951a3f39e449756d0b685980f9237ff
mah man <muthl@github.com>
2018-05-14 11:11:20 -0700

    more

---
+++ Functions/src/Function.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Function   {
+    private double m = 3.7, num = 15;
+    public double total = 0;
+    public Function() {
+    }
+    public double getnum() {
+        return num;
+
+    }
+}
+
+
+
+
+
---
+++ Functions/src/Tester.java
@@ -0,0 +1,9 @@
+
+public class Tester {
+
+    public static void main(String[] args) {
+        Function c1 = new Function();
+        System.out.println("Number is:" + c1.getnum());
+    }
+
+}
